045289e24f289333a45491a792d8ca9009262902
carlos andres alavez <carlos_caas@hotmail.com>
2024-11-09 00:53:17 -0600

    Proyecto integrado

---
+++ blazorAppDemo/Services/CategoryService.cs
@@ -0,0 +1,34 @@
+using System.Text.Json;
+
+namespace blazorappdemo;
+
+public class CategoryService : ICategoryService
+{
+    private readonly HttpClient client;
+    private readonly JsonSerializerOptions options;
+
+    public CategoryService(HttpClient httpClient)
+    {
+        this.client = httpClient;
+        options = new JsonSerializerOptions { PropertyNameCaseInsensitive=true};
+    }
+
+    public async Task<List<Category>?>Get()
+    {
+        var response = await client.GetAsync("v1/categories");
+        var content = await response.Content.ReadAsStringAsync();
+
+        if(!response.IsSuccessStatusCode)
+        {
+            throw new ApplicationException(content);
+        }
+
+        return JsonSerializer.Deserialize<List<Category>>(content,options);
+    }
+    
+}
+
+public interface ICategoryService
+{
+    Task<List<Category>?>Get();
+}
